6f01e4518b19576b933edaa6f1466d7253916361
Winnie Mui <winnie_mui53@hotmail.com>
2018-03-17 15:28:31 -0700

    Included spec

--- src/update_keyboard.py
+++ src/update_keyboard.py
@@ -1,1 +1,14 @@
-# Update function code goes here
+# Update keyboard function to send the updated keycodes and bitmaps to a microcontroller
+# Transfers the data using the python XMODEM library
+
+# Inputs: key_list - a list of key_character objects
+# Outputs: Sends the key_character data in the keylist to the connected microcontroller
+
+# TODO: Tal look into how COM ports are recognized and addressed
+#       ideally this would involve reading all the device COM ports
+#       and finding the one that matches the device name. Ask David
+#       for the device name
+#       If COM port stuff is complicated enough we may want to look into making it user-selectable
+
+def update_keyboard(key_list)
+    
