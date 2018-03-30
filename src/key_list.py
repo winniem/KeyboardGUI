9638cd1b955e25e4c9db8c589c72095a09231af4
Winnie Mui <winnie_mui53@hotmail.com>
2018-03-29 18:07:51 -0700

    Included offset in generated bitmaps

---
+++ src/key_list.py
@@ -0,0 +1,16 @@
+#Key list class, has a naem.
+
+class key_list:
+
+    def __init__(self, keychars, name):
+        self.keychars = keychars
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def get_keychars(self):
+        return self.get_keychars
+
+    def update_keychars(self, keychars):
+        self.keychars = keychars
